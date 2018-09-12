24a8232115272c7e484bca63e5a41f9419050581
xixex <mikhailsontsav@gmail.com>
2018-09-12 16:52:14 +0300

    zeros done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
-}
+    var res = 0;
+    while (number >= 5) {
+        res += Math.floor(number / 5);
+        number = Math.floor(number / 5);
+    }
+    return res;
+};
